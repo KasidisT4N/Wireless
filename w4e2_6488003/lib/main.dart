91db50ffc8507c39a7644327603f8b53cdcd2ec5
Kasidis Aiamsamarng <kasidis.aia@student.mahidol.edu>
2024-02-04 17:32:26 +0700

    main

--- w4e2_6488003/lib/main.dart
+++ w4e2_6488003/lib/main.dart
@@ -1,125 +1,364 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FigmaToCodeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Thank you: https://www.figma.com/community/plugin/842128343887142055/
+class FigmaToCodeApp extends StatelessWidget {
+  const FigmaToCodeApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: ListView(children: [
+          Confirm(),
+        ]),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Confirm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Column(
+      children: [
+        Container(
+          width: 393,
+          height: 852,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(color: Colors.white),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 28,
+                top: 102,
+                child: Text(
+                  'Papaya Salad(Best Offer)',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 26,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    height: 0.05,
+                    letterSpacing: -0.57,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 26,
+                top: 191,
+                child: SizedBox(
+                  width: 167,
+                  height: 26,
+                  child: Text(
+                    'Papaya Salad\n',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      height: 0.07,
+                      letterSpacing: -0.44,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 220,
+                top: 140,
+
+                  child: Image(
+                image: AssetImage('images/som.jpg'),
+                width: 120.0, // Set the width of the image
+                height: 120.0, // Set the height of the image
+                fit: BoxFit.cover, // Adjust the BoxFit property as needed
+              )
+              ),
+              Positioned(
+                left: 26,
+                top: 266,
+                child: Container(
+                  width: 119,
+                  height: 38,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFFCD36A),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(49.99),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 114,
+                top: 285,
+                child: SizedBox(
+                  width: 18,
+                  height: 31,
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.17,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      height: 0.06,
+                      letterSpacing: -0.55,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 74,
+                top: 273,
+                child: Container(
+                  width: 24,
+                  height: 24,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: OvalBorder(),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 83,
+                top: 285,
+                child: Text(
+                  '1',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 19.74,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    height: 0.08,
+                    letterSpacing: -0.43,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 305,
+                top: 285,
+                child: SizedBox(
+                  width: 60,
+                  height: 27,
+                  child: Text(
+                    '\$20.99',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                      height: 0.08,
+                      letterSpacing: -0.40,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 23,
+                top: 385,
+                child: Container(
+                  width: 345,
+                  height: 78,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x1C000000),
+                        blurRadius: 20,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 42,
+                top: 410,
+                child: Text(
+                  '2 pc. Grill chicken',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    height: 0.08,
+                    letterSpacing: -0.40,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 42,
+                top: 440,
+                child: SizedBox(
+                  width: 289,
+                  child: Text(
+                    'Add 1 for free with \$5.67 purchase',
+                    style: TextStyle(
+                      color: Color(0xFFFCD36A),
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                      height: 0.09,
+                      letterSpacing: -0.35,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 320,
+                top: 402,
+                child: Container(
+                  width: 29,
+                  height: 29,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFFBDE3F),
+                    shape: OvalBorder(),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 328,
+                top: 416,
+                child: SizedBox(
+                  width: 18,
+                  height: 31,
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.17,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      height: 0.06,
+                      letterSpacing: -0.55,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 26,
+                top: 497,
+                child: Text(
+                  'Subtotal',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    height: 0.07,
+                    letterSpacing: -0.44,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 297,
+                top: 498,
+                child: SizedBox(
+                  width: 68,
+                  height: 27,
+                  child: Text(
+                    '\$20.99',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      height: 0.07,
+                      letterSpacing: -0.44,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 26,
+                top: 693,
+                child: Container(
+                  width: 342,
+                  height: 55,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFFCD36A),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 160,
+                top: 720,
+                child: SizedBox(
+                  width: 129,
+                  child: Text(
+                    'Go back',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 17,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                      height: 0.09,
+                      letterSpacing: -0.37,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 26,
+                top: 770,
+                child: Container(
+                  width: 342,
+                  height: 55,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: 342,
+                          height: 55,
+                          decoration: ShapeDecoration(
+                            color: Color(0xFFFCD36A),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 134,
+                        top:25,
+                        child: SizedBox(
+                          width: 86,
+                          child: Text(
+                            'Confirm',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              height: 0.09,
+                              letterSpacing: -0.37,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
